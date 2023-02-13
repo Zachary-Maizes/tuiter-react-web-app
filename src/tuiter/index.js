284db99506d9ccbee28254ecb0e88cfeabacc1e3
Zachary-Maizes <zmaizes@gmail.com>
2023-02-13 18:13:15 -0500

    A6 - Added Nav hrefs

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,7 +1,5 @@
-import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
-import PostSummaryList from "./post-summary-list";
 import ExploreComponent from "./explore";
 
 function Tuiter() {
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -18,46 +18,46 @@ const NavigationSidebar = (
 ) => {
   return (
     <div className="list-group">
-      <a className="list-group-item">
+      <a className="list-group-item" href="#Tuiter">
         <FontAwesomeIcon icon={faTwitter} fixedWidth />
       </a>
       <a className={`list-group-item
-                    ${active === 'home'?'active':''}`}>
+                    ${active === 'home'?'active':''}`} href="#Home">
         <FontAwesomeIcon icon={faHome} fixedWidth />
         <p className="d-none d-xl-inline">Home</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`}>
+                    ${active === 'explore'?'active':''}`} href="#Explore">
         <FontAwesomeIcon icon={faHashtag} fixedWidth />
         <p className="d-none d-xl-inline">Explore</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`}>
+                    ${active === 'notifications'?'active':''}`} href="#Notifications">
         <FontAwesomeIcon icon={faBell} fixedWidth />
         <p className="d-none d-xl-inline">Notifications</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`}>
+                    ${active === 'messages'?'active':''}`} href="#Messages">
         <FontAwesomeIcon icon={faEnvelope} fixedWidth />
         <p className="d-none d-xl-inline">Messages</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`}>
+                    ${active === 'bookmarks'?'active':''}`} href="#Bookmarks">
         <FontAwesomeIcon icon={faBookmark} fixedWidth />
         <p className="d-none d-xl-inline">Bookmarks</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`}>
+                    ${active === 'lists'?'active':''}`} href="#Lists">
         <FontAwesomeIcon icon={faList} fixedWidth />
         <p className="d-none d-xl-inline">Lists</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`}>
+                    ${active === 'profile'?'active':''}`} href="#Profile">
         <FontAwesomeIcon icon={faUser} fixedWidth />
         <p className="d-none d-xl-inline">Profile</p>
       </a>
       <a className={`list-group-item
-                    ${active === 'more'?'active':''}`}>
+                    ${active === 'more'?'active':''}`} href="#More">
         <FontAwesomeIcon icon={faCircleChevronDown} fixedWidth />
         <p className="d-none d-xl-inline">More</p>
       </a>
